9d000a262570d89b3a51e857bfa0780017921b77
marlowl <marlowlieshout@hotmail.com>
2022-05-16 20:29:16 +0200

    getMyVote function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import './App.css';
+import { Client as Styletron } from 'styletron-engine-atomic';
+import { Provider as StyletronProvider } from 'styletron-react';
+import { LightTheme, BaseProvider } from 'baseui';
+import { Button } from 'baseui/button';
+const { ApiPromise, WsProvider } = require("@polkadot/api");
+const { ContractPromise } = require("@polkadot/api-contract");
+const { keyring } = require("@polkadot/ui-keyring");
+keyring.loadAll({ ss58Format: 42, type: "sr25519" });
 
-function App() {
-  return (
-    <div className="App">
-    </div>
+const engine = new Styletron();
+
+const getMyVote = async () => {
+  const provider = new WsProvider("ws://127.0.0.1:9944");
+  const api = await ApiPromise.create({ provider });
+  const abi = require("./metadata.json");
+
+  const contractAddress = "5FkfsfMv7TaLSu5qiKANN2sZucsmuBmERqNQyJ4vqDUZ7XYg";
+  const contract = new ContractPromise(api, abi, contractAddress);
+
+  const alicePair = keyring.createFromUri("//Alice");
+
+  const value = 0;
+  const gasLimit = 1000000000000;
+  const { output } = await contract.query.getMyVote(
+      alicePair.address,
+      { value, gasLimit }
   );
+  console.log(output.toHuman())
 }
 
-export default App;
+export default function Hello() {
+  return (
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={LightTheme}>
+          <Button onClick={() => getMyVote()}>Get my vote</Button>
+      </BaseProvider>
+    </StyletronProvider>
+  );
+}
